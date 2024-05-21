98bdbfc4e2d2624688df0117b4ed1382689d8871
javohirgvdev <gayratovjavohir90@gmail.com>
2024-05-21 23:22:45 +0500

    I added html, css and js

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+
+//   <swiper-container class="mySwiper" pagination="true" pagination-clickable="true" space-between="30"
+//                     slides-per-view="3">
+//                     
+//                     <div class="swiper-button-next"></div>
+//                     <div class="swiper-button-prev"></div>
+//                     <div class="swiper-pagination"></div>
+//                 </swiper-container>
